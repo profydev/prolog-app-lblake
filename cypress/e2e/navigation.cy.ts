e9285a0b04a41d22c5b8f624a1abfc137416a682
lblake <treleven.lloyd@gmail.com>
2023-12-18 17:42:31 +0000

    updated test

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -45,7 +45,7 @@ describe("Sidebar Navigation", () => {
 
       // check that links still exist and are functionable
       cy.get("nav").find("a").should("have.length", 6).eq(1).click();
-      cy.url().should("eq", "http://localhost:3000/dashboard/issues");
+      cy.url().should("eq", "http://localhost:3000/dashboard");
 
       // check that text is not rendered
       cy.get("nav").contains("Issues").should("not.exist");
